2647dec31182b8521119a554017fa9c61e79ed16
harktank <ghodehimanshu453@gmail.com>
2024-07-17 12:55:50 +0530

    new

--- src/components/home/Hprice.jsx
+++ src/components/home/Hprice.jsx
@@ -6,7 +6,7 @@ const Hprice = () => {
   return (
     <>
       <section className='hprice padding'>
-        <Heading subtitle='OUR PRICING' title='Pricing & Packages' />
+        <Heading subtitle='OUR Process' title='Step by Step Guide' />
         <div className='price container grid'>
           <PriceCard />
         </div>
